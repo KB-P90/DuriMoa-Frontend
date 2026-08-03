02695fa10426d0a0f318fec4f1ac12dc9a8383ac
ywJung <project.yw.103@gmail.com>
2026-08-03 15:52:22 +0900

    fix: merge 하면서 import 누락되어 수정
    - api 요청 문제 있어서 해결 중이었는데 axios 관련 보완 사항 있다고 해서 다시 merge 후 재진행

--- router/index.js
+++ router/index.js
@@ -10,6 +10,7 @@ import CalendarPageView from '@/views/CalendarPageView.vue';
 import PlaceholderView from '@/views/PlaceholderView.vue';
 import HomeView from '@/views/HomeView.vue';
 import OnboardingView from '@/views/onboarding/OnboardingView.vue';
+import MonthlyExpenseView from '@/views/MonthlyExpenseView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
