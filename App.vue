eb5f4756a131a4d93d2cb3b4d92728bd5b555e31
1juyoung <myjuyoung2@gmail.com>
2026-08-04 16:17:25 +0900

    chore: 토스트(vue-sonner) 도입 및 다이얼로그 스크롤 락 해제
    npx shadcn-vue add sonner로 Toaster 컴포넌트를 추가하고 App.vue 루트에
    붙인다. vue-sonner는 CSS를 따로 import해줘야 해서 main.css에 추가했다.
    또한 reka-ui의 AlertDialog가 열릴 때 배경 스크롤을 잠그면서 body에
    padding-right를 자동으로 넣는데, body 자체가 max-w-[768px] mx-auto로
    중앙 정렬된 컬럼이라 이 보정이 오히려 레이아웃을 흔들어서
    ConfigProvider(:scroll-body="false")로 꺼둔다.

--- App.vue
+++ App.vue
@@ -1,3 +1,11 @@
+<script setup>
+import { ConfigProvider } from 'reka-ui';
+import { Toaster } from '@/components/ui/sonner';
+</script>
+
 <template>
-  <RouterView />
+  <ConfigProvider :scroll-body="false">
+    <RouterView />
+    <Toaster position="top-center" />
+  </ConfigProvider>
 </template>
